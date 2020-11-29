096151df1e048ddce77ae9f66d59140228bdcb1f
Bak Balázs <bak.balazs95@gmail.com>
2020-11-30 00:12:45 +0100

    databinding használta a bejelentkezési képernyőn

--- app/src/main/java/hu/unideb/shoppinglist/LoginActivity.kt
+++ app/src/main/java/hu/unideb/shoppinglist/LoginActivity.kt
@@ -1,22 +1,114 @@
 package hu.unideb.shoppinglist
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.ktx.Firebase
+import hu.unideb.shoppinglist.databinding.ActivityLoginBinding
 import hu.unideb.shoppinglist.pages.HomeActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var viewModel1: LoginViewModel
+
+// ...
+// Initialize Firebase Auth
+
 //    private lateinit var viewModel: LoginViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+//    val loginModel:LoginViewModel by viewModels()
+
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+//    binding.viewModel = loginModel;
+
+
+    viewModel1 = ViewModelProvider(this@LoginActivity).get(LoginViewModel::class.java)
+
+    binding.viewModel = viewModel1
+
+//    binding.setLifecycleOwner(this)
+
+//                 auth = Firebase.auth
+
+//    auth.createUserWithEmailAndPassword("bak.1@gmail.com", "test1111A")
+//        .addOnCompleteListener(this) { task ->
+//            if (task.isSuccessful) {
+//                // Sign in success, update UI with the signed-in user's information
+//                Log.d("TAG", "createUserWithEmail:success")
+//                val user = auth.currentUser
+////                FirebaseUser user22 = FirebaseAuth.getInstance().getCurrentUser();
+//
+//                val profileUpdates = userProfileChangeRequest {
+//                    displayName = "Jane Q. User"
+////                    photoUri = Uri.parse("https://example.com/jane-q-user/profile.jpg")
+//                }
+//
+//                user!!.updateProfile(profileUpdates)
+//                    .addOnCompleteListener { task ->
+//                        if (task.isSuccessful) {
+//                            Log.d("TAG", "User profile updated.")
+//                        }
+//                    }
+////                user?.updateProfile({
+////                        displayName: newUser.name
+////                })
+//                Log.w("TAG", user.toString());
+////                updateUI(user)
+//            } else {
+//                // If sign in fails, display a message to the user.
+//                Log.w("TAG", "createUserWithEmail:failure", task.exception)
+////                Toast.makeText(baseContext, "Authentication failed.",
+////                    Toast.LENGTH_SHORT).show()
+////                updateUI(null)
+//            }
+//
+//            // ...
+//        }
+
+
+//    auth.signInWithEmailAndPassword("bak.1@gmail.com", "test1111A")
+//        .addOnCompleteListener(this) { task ->
+//            if (task.isSuccessful) {
+//                // Sign in success, update UI with the signed-in user's information
+//                Log.d("TAG", "signInWithEmail:success")
+//                val user = auth.currentUser
+//                println(user?.displayName)
+//                println(user?.email)
+//
+////                updateUI(user)
+//            } else {
+//                // If sign in fails, display a message to the user.
+//                Log.w("TAG", "signInWithEmail:failure", task.exception)
+////                Toast.makeText(baseContext, "Authentication failed.",
+////                    Toast.LENGTH_SHORT).show()
+////                updateUI(null)
+//                // ...
+//            }
+//
+//            // ...
+//        }
+
 //        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
 
 
-        setContentView(R.layout.login_layout)
+//        setContentView(R.layout.activity_login)
 
 //        val button = findViewById<Button>(R.id.button2)
 //        button.setOnClickListener {
@@ -28,14 +120,15 @@ class LoginActivity : AppCompatActivity() {
 //            val intent = Intent(this, HomeActivity::class.java)
 //            startActivity(intent)
 //        }
+   
     }
 
-    fun goToHome() {
-
-    }
-
-    fun goToHome(view: View) {
-        val intentAct: Intent =Intent(this@LoginActivity, HomeActivity::class.java)
-        startActivity(intentAct)
-    }
+//    fun goToHome() {
+//
+//    }
+//
+//    fun goToHome(view: View) {
+//        val intentAct: Intent =Intent(this@LoginActivity, HomeActivity::class.java)
+//        startActivity(intentAct)
+//    }
 }
--- app/src/main/java/hu/unideb/shoppinglist/LoginViewModel.kt
+++ app/src/main/java/hu/unideb/shoppinglist/LoginViewModel.kt
@@ -1,14 +1,18 @@
 package hu.unideb.shoppinglist
 
+import android.app.Application
 import android.content.Context
 import android.content.Intent
+import android.view.View
 import androidx.core.content.ContextCompat.startActivity
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import hu.unideb.shoppinglist.pages.HomeActivity
 
-class LoginViewModel:ViewModel() {
+class LoginViewModel():ViewModel() {
 
-    fun goToHomePage(context: Context) {
+    fun goToHomePage(view: View) {
+                val context:Context = view.context;
             val intentAct: Intent =Intent(context, HomeActivity::class.java)
             context.startActivity(intentAct)
     }
